c791c4475e9c232d7fa13537c6f25d61533d9525
GitHub Action <action@github.com>
2020-09-14 12:10:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/minidigger/firstspiritlanguagesupport/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.minidigger.firstspiritlanguagesupport.services
+
+import com.github.minidigger.firstspiritlanguagesupport.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/minidigger/firstspiritlanguagesupport/services/MyProjectService.kt
+++ src/main/kotlin/com/github/minidigger/firstspiritlanguagesupport/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.minidigger.firstspiritlanguagesupport.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.minidigger.firstspiritlanguagesupport.MyBundle
 
 class MyProjectService(project: Project) {
 
